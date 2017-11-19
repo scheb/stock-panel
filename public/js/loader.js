5750302adace599ba35879829d7559fdd0d0db23
Christian Scheb <info@stylemotion.de>
2017-11-19 21:10:09 +0100

    Fix chart rendering

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,42 +1,95 @@
+function setPrivacy(isEnabled) {
+    $.cookie('privacy', isEnabled ? 1 : 0, { expires: 360 });
+    if (isEnabled) {
+        $(".privacy").hide();
+        $(".btn-privacy .glyphicon").addClass("glyphicon-eye-close").removeClass("glyphicon-eye-open");
+    }
+    else {
+        $(".privacy").show();
+        $(".btn-privacy .glyphicon").addClass("glyphicon-eye-open").removeClass("glyphicon-eye-close");
+    }
+}
+
+function togglePrivacy() {
+    if ($.cookie('privacy') == "1") {
+        setPrivacy(false);
+    }
+    else {
+        setPrivacy(true);
+    }
+}
+
+function getChartDataUrl(stockId, period) {
+    return '/charts/' + stockId + '/' + period  + '.json?callback=?';
+}
+
+function initStockChart(stockId) {
+    $.getJSON(getChartDataUrl(stockId, "1d"), function (data) {
+        Highcharts.stockChart("chart-" + stockId, {
+            chart: {
+                zoomType: false
+            },
+            navigator: {
+                enabled: false
+            },
+            scrollbar: {
+                enabled: false
+            },
+            rangeSelector: {
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                type: "datetime",
+                minRange: 3600 * 1000 // one hour
+            },
+            yAxis: {
+                floor: 0
+            },
+            series: [{
+                name: 'Price',
+                data: data,
+                dataGrouping: {
+                    enabled: false
+                },
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
+}
+
+function changeStockChartsPeriod(period) {
+    $('#stock-charts .chart').each(function () {
+        var chart = $(this);
+        var highcharts = chart.highcharts();
+        var stockId = chart.attr('data-stock-id');
+        if (highcharts) {
+            highcharts.showLoading('Loading data from server...');
+            $.getJSON(getChartDataUrl(stockId, period), function (data) {
+                highcharts.series[0].setData(data);
+                highcharts.hideLoading();
+            });
+        }
+    });
+}
+
 $(document).ready(function() {
-	//External links
-	$('a[rel*=external]').click( function() {
-		window.open(this.href);
-		return false;
-	});
-
-    var setPrivacy = function(isEnabled) {
-        $.cookie('privacy', isEnabled ? 1 : 0, { expires: 360 });
-        if (isEnabled) {
-            $(".privacy").hide();
-            $(".btn-privacy .glyphicon").addClass("glyphicon-eye-close").removeClass("glyphicon-eye-open");
-        }
-        else {
-            $(".privacy").show();
-            $(".btn-privacy .glyphicon").addClass("glyphicon-eye-open").removeClass("glyphicon-eye-close");
-        }
-    };
-
-    var togglePrivacy = function() {
-        if ($.cookie('privacy') == "1") {
-            setPrivacy(false);
-        }
-        else {
-            setPrivacy(true);
-        }
-    };
+    //External links
+    $('a[rel*=external]').click( function() {
+        window.open(this.href);
+        return false;
+    });
 
     $(".btn-privacy").click(togglePrivacy);
     setPrivacy($.cookie('privacy') == "1");
 
     // Charts overview only
-    $('#period-tabs a').click(function (e) {
-        // e.preventDefault();
+    $('#period-tabs a').click(function () {
         var period = $(this).attr('data-period');
-        $('#stock-charts .chart').each(function () {
-            var imageUrl = $(this).attr('data-image-url').replace(/\{period\}/, period);
-            console.log(this, imageUrl);
-            $(this).css('background-image', 'url(' + imageUrl + ')');
-        });
+        changeStockChartsPeriod(period);
     });
 });
--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -2,13 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Stock;
 use App\Provider\StockPriceProvider;
+use App\Repository\StockRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanelController extends Controller
 {
+    const RANGE_INTERVAL_MAP = [
+        '1d' => '2m',
+        '5d' => '15m',
+        '1mo' => '1h',
+        '6mo' => '1d',
+        'ytd' => '1wk',
+        '1y' => '1wk',
+        '5y' => '1mo',
+        'max' => '1mo',
+    ];
+
     /**
      * Show the stock panel
      * @Route("/", name="stock_panel")
@@ -36,6 +53,57 @@ class PanelController extends Controller
     }
 
     /**
+     * Get JSON data for the chart
+     * @Route("/charts/{id}/{range}.json", name="stock_charts_data")
+     */
+    public function getChartData(int $id, string $range, Request $request): Response
+    {
+        $stock = $this->getStock($id);
+        if (!$stock) {
+            throw $this->createNotFoundException('Stock not found');
+        }
+        if (!array_key_exists($range, self::RANGE_INTERVAL_MAP)) {
+            throw $this->createNotFoundException('Invalid range');
+        }
+
+        $symbol = $stock->getSymbol();
+        $interval = self::RANGE_INTERVAL_MAP[$range];
+
+        $url = "https://query1.finance.yahoo.com/v8/finance/chart/" . $symbol . "?range=" . $range . "&includePrePost=false&interval=" . $interval;
+        $client = new Client();
+        try {
+            $response = $client->get($url);
+        } catch (ClientException $e) {
+            throw $this->createNotFoundException('Client error: ' . $e->getMessage(), $e);
+        }
+
+        $json = json_decode($response->getBody(), true);
+        if (!isset($json['chart']['result'][0]['timestamp'])) {
+            throw $this->createNotFoundException('Timestamps not found');
+        }
+        if (!isset($json['chart']['result'][0]['indicators']['quote'][0]['close'])) {
+            throw $this->createNotFoundException('Closing pricees not found');
+        }
+
+        $data = [];
+        $timestamps = $json['chart']['result'][0]['timestamp'];
+        $closingPrices = $json['chart']['result'][0]['indicators']['quote'][0]['close'];
+        $previous = null;
+        foreach ($timestamps as $index => $timestamp) {
+            $price = $closingPrices[$index] ?? $previous;
+            $previous = $price;
+            $data[] = [$timestamp * 1000, $price];
+        }
+
+        $callback = $request->get('callback', '?');
+        $content = $callback . '(' . json_encode($data) . ');';
+        $response = new Response($content);
+        $response->headers->set('Content-Type', 'application/javascript');
+        $response->setExpires(new \DateTime('+1 hour'));
+        return $response;
+    }
+
+    /**
      * Force stock update
      * @Route("/update", name="stock_update")
      */
@@ -48,6 +116,21 @@ class PanelController extends Controller
             'stocks' => $stocks,
             'privacyMode' => true,
         ]);
+    }
+
+    private function getStock(int $id): ?Stock
+    {
+        return $this->getStockRepo()->findOneById($id);
+    }
+
+    private function getObjectManager(): ObjectManager
+    {
+        return $this->getDoctrine()->getManager();
+    }
+
+    private function getStockRepo(): StockRepository
+    {
+        return $this->getObjectManager()->getRepository(Stock::class);
     }
 
     private function getStockPriceProvider(): StockPriceProvider
