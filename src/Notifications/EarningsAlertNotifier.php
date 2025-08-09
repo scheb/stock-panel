a8adb4b6aa265d857d45fb095f6c0c62af0ce44f
Christian Scheb <privat@stylemotion.de>
2025-08-09 21:48:32 +0200

    Attach calendar events to earnings notifications

--- src/Notifications/EarningsAlertNotifier.php
+++ src/Notifications/EarningsAlertNotifier.php
@@ -6,13 +6,22 @@ namespace App\Notifications;
 
 use App\Entity\Stock;
 use App\Provider\StockEarningsProvider;
-use App\Provider\StockUpdateEvent;
-use App\Repository\StockRepository;
+use DateInterval;
 use Doctrine\ORM\EntityManagerInterface;
+use Eluceo\iCal\Domain\Entity\Attendee;
+use Eluceo\iCal\Domain\Entity\Calendar;
+use Eluceo\iCal\Domain\Entity\Event;
+use Eluceo\iCal\Domain\ValueObject\Alarm;
+use Eluceo\iCal\Domain\ValueObject\Alarm\DisplayAction;
+use Eluceo\iCal\Domain\ValueObject\Alarm\RelativeTrigger;
+use Eluceo\iCal\Domain\ValueObject\DateTime;
+use Eluceo\iCal\Domain\ValueObject\EmailAddress;
+use Eluceo\iCal\Domain\ValueObject\TimeSpan;
+use Eluceo\iCal\Presentation\Factory\CalendarFactory;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\DataPart;
 use Twig\Environment;
 
 class EarningsAlertNotifier
@@ -48,30 +57,50 @@ class EarningsAlertNotifier
                 continue;
             }
 
-            $notifyAboutEarnings[] = $stock;
+            $this->notify($stock);
+            $stock->setEarningsNotificationLastTime(new \DateTime());
+            $this->em->persist($stock);
         }
 
-        if ($notifyAboutEarnings) {
-            $this->notify($notifyAboutEarnings);
-        }
+        $this->em->flush();
     }
 
-    private function notify(array $stocks): void
+    private function notify(Stock $stock): void
     {
-        $text = $this->twig->render('Notifications/earningsAlert.txt.twig', ['stocks' => $stocks]);
+        $text = $this->twig->render('Notifications/earningsAlert.txt.twig', ['stock' => $stock]);
         $subject = substr($text, 0, strpos($text, "\n"));
+
+        $event = new Event();
+        $eventTime = new DateTime($stock->getNextEarningsTime(), true);
+        $event
+            ->setSummary($stock->getName() . ' Earnings')
+            ->setDescription($text)
+            ->setOccurrence(new TimeSpan($eventTime, $eventTime))
+            ->addAttendee(new Attendee(new EmailAddress($this->recipient)))
+            ->addAlarm(new Alarm(
+                new DisplayAction('Reminder: '.$stock->getName().' Earnings in 12 hours!'),
+                new RelativeTrigger(DateInterval::createFromDateString('-12 hours'))->withRelationToEnd()
+            ))
+            ->addAlarm(new Alarm(
+                new DisplayAction('Reminder: '.$stock->getName().' Earnings in 15 minutes!'),
+                new RelativeTrigger(DateInterval::createFromDateString('-15 minutes'))->withRelationToEnd()
+            ))
+        ;
+
+        $calendar = new Calendar([$event]);
+        $ics = new CalendarFactory()->createCalendar($calendar);
 
         $email = new Email()
             ->from($this->sender)
             ->to($this->recipient)
             ->subject($subject)
             ->text($text);
-        $this->mailer->send($email);
 
-        foreach ($stocks as $stock) {
-            $stock->setEarningsNotificationLastTime(new \DateTime());
-            $this->em->persist($stock);
-        }
-        $this->em->flush();
+        $attachment = new DataPart((string) $ics, 'invite.ics', 'text/calendar', 'quoted-printable');
+        $attachment->asInline();
+        $attachment->getHeaders()->addParameterizedHeader('Content-Type', 'text/calendar', ['charset' => 'utf-8', 'method' => 'REQUEST']);
+        $email->addPart($attachment);
+
+        $this->mailer->send($email);
     }
 }
