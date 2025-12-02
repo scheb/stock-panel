4a9c36abbd2512d3ee5ecd7622b8e7bfe40964ba
Christian Scheb <privat@stylemotion.de>
2025-12-02 13:42:52 +0100

    Upgrade to Symfony 8

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class PanelController extends AbstractController
 {
